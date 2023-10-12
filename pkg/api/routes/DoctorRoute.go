5c2117815ed137c601c3fbc6d9d8fd4de27a272f
kannan112 <kannangt100@gmail.com>
2023-10-12 17:37:21 +0530

    doctor slot booking route

--- pkg/api/routes/DoctorRoute.go
+++ pkg/api/routes/DoctorRoute.go
@@ -12,9 +12,13 @@ func DoctorSetUpRoute(engine *gin.Engine, doctorHandler *handler.DoctorHandler) 
 		doctor.POST("login", doctorHandler.Login) // get block because its not approved by admin
 		doctor.POST("signup/:categoryid", doctorHandler.DoctorRegistration)
 		doctor.GET("/categorylist", doctorHandler.ListCategory)
-		test := doctor.Group("/profile", middleware.DoctorAuth)
+		Profile := doctor.Group("/profile", middleware.DoctorAuth)
 		{
-			test.GET("/", doctorHandler.Profile)
+			Profile.GET("/", doctorHandler.Profile)
+		}
+		Appointment := doctor.Group("/slot")
+		{
+			Appointment.POST("/add")
 		}
 	}
 }
